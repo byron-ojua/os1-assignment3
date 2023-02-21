ea95fba94087c71a33a0db603682b9ab3bdf75b1
Byron OjuaNice <niceb@os1.engr.oregonstate.edu>
2023-02-21 01:43:44 -0800

    Added header file

---
+++ header.h
@@ -0,0 +1,49 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <dirent.h>
+#include <signal.h>
+
+#define BUFFER_SIZE 2048
+#define CMD_EXIT "exit"
+#define CMD_ECHO "echo"
+
+
+/**
+ * @brief Basic structure for holding command parameters
+ * 
+ */
+struct command{
+    char *cmd;
+    int background;
+    int in;
+    char *input;
+    int out;
+    char *output;
+    char args[512][BUFFER_SIZE];
+    int numArgs;
+};
+
+void getcmd(char *cmdLine);
+int checkExit(char *cmdLine);
+int skipCmd(char *cmdLine);
+int isEcho(char *cmdLine);
+void printCmd(struct command *cmd);
+struct command *createCmd(char *cmdLine);
+int isStatus(int last);
+int isBuiltIn(char *cmdLine, int *status);
+void setIOStreams(struct command *cmd);
+void runCmd(char *cmdLine, int *lastStatus);
+
+#endif // !HEADER_H
